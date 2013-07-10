74dddf375656fe800567712654bd0841a448446d
John Grubbs <rand4505@yahoo.com>
2013-07-10 16:49:19 -0500

    Create GetString.cs
    GetString form.

---
+++ GetString.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AESEncryptAndDecrypt
+{
+    public partial class GetString : Form
+    {
+        private string _Password;
+        public string Password { get { return _Password; } set { _Password = value; } }
+
+        public GetString()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Password = textBox1.Text.Trim();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
