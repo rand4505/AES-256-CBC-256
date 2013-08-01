5a0cc2bb5d55b46ce37b5f8031dab19978142e67
John Grubbs <rand4505@yahoo.com>
2013-08-01 17:43:28 -0500

    Update myAES.cs

--- myAES.cs
+++ myAES.cs
@@ -14,18 +14,24 @@ namespace AESEncryptAndDecrypt
     {
         private byte[] mkey = { 202, 42, 168, 24, 48, 57, 76, 72, 113, 215, 114, 12, 17, 12, 152, 109, 41, 85, 90, 70, 68, 184, 196, 134, 175, 129, 117, 235, 135, 176, 153, 145 };
         public byte[] key{ set { mkey = value; } get { return mkey; } }
-        private byte[] mvector = { 244, 74, 91, 11, 223, 37, 57, 145, 64, 135, 42, 57, 76, 28, 14, 116, 244, 74, 91, 11, 223, 37, 57, 145, 64, 135, 42, 57, 76, 28, 14, 116 };
+        private byte[] mvector = { 244, 74, 91, 11, 223, 37, 57, 145, 64, 135, 42, 57, 76, 28, 14, 116/*, 244, 74, 91, 11, 223, 37, 57, 145, 64, 135, 42, 57, 76, 28, 14, 116 */};
         public byte[] vector{ get { return mvector; } set { mvector = value; } }
+        public enum Algo { AES256CBC256, AES256CBC128, AES256CFB128, AES128CBC128};
+        private string[] AlgoArrayComp = { "256-AES-256-CBC", "256-AES-128-CBC(Nist Rijndael)", "256-AES-128-CFB", "128-AES-128-CBC" };
+        private Algo AlgorithmChoice = new Algo();
         private ICryptoTransform encryptor, decryptor;
         private UTF8Encoding encoder;
-        private RijndaelManaged rm;
+        private static RijndaelManaged rm;
+        private static int kSize, vSize;
         private static string Salt = "I see THE siloutta of a MAN, skallamoo skallamoo TILL you do THE fandango, thunderbolts AND lightning, NOT as frightning as the NSA";
         private const int SizeOfBuffer = 1024 * 8;
 
         public SimplerAES() {
             rm = new RijndaelManaged();
-            rm.KeySize = 256;
-            rm.BlockSize = 256;
+            kSize = 256;
+            vSize = 128;
+            rm.KeySize = kSize;
+            rm.BlockSize = vSize;
             encryptor = rm.CreateEncryptor(mkey, mvector);
             decryptor = rm.CreateDecryptor(mkey, mvector);
             encoder = new UTF8Encoding();
@@ -33,11 +39,69 @@ namespace AESEncryptAndDecrypt
 
         public SimplerAES(byte[] InnerK, byte[] InnerV) {
             rm = new RijndaelManaged();
-            rm.KeySize = 256;
-            rm.BlockSize = 256;
+            kSize = 256;
+            vSize = 128;
+            rm.KeySize = kSize;
+            rm.BlockSize = vSize;
             encryptor = rm.CreateEncryptor(mkey, mvector);
             decryptor = rm.CreateDecryptor(mkey, mvector);
             encoder = new UTF8Encoding();
+        }
+
+        public SimplerAES(Algo UserChoiceAlgo, byte[] InnerK, byte[] InnerV) {
+            AlgorithmChoice = UserChoiceAlgo;
+            if (AlgorithmChoice == Algo.AES128CBC128) {
+                rm = new RijndaelManaged();
+                kSize = 128;
+                vSize = 128;
+                rm.KeySize = kSize;
+                rm.BlockSize = vSize;
+                rm.Mode = CipherMode.CBC;
+                mkey = InnerK;
+                mvector = InnerV;
+                encryptor = rm.CreateEncryptor(mkey, mvector);
+                decryptor = rm.CreateDecryptor(mkey, mvector);
+                encoder = new UTF8Encoding();
+            }
+            else if (AlgorithmChoice == Algo.AES256CBC128) {
+                rm = new RijndaelManaged();
+                kSize = 256;
+                vSize = 128;
+                rm.KeySize = kSize;
+                rm.BlockSize = vSize;
+                rm.Mode = CipherMode.CBC;
+                mkey = InnerK;
+                mvector = InnerV;
+                encryptor = rm.CreateEncryptor(mkey, mvector);
+                decryptor = rm.CreateDecryptor(mkey, mvector);
+                encoder = new UTF8Encoding();
+            }
+            else if (AlgorithmChoice == Algo.AES256CBC256) {
+                rm = new RijndaelManaged();
+                kSize = 256;
+                vSize = 256;
+                rm.KeySize = kSize;
+                rm.BlockSize = vSize;
+                rm.Mode = CipherMode.CBC;
+                mkey = InnerK;
+                mvector = InnerV;
+                encryptor = rm.CreateEncryptor(mkey, mvector);
+                decryptor = rm.CreateDecryptor(mkey, mvector);
+                encoder = new UTF8Encoding();
+            }
+            else if (AlgorithmChoice == Algo.AES256CFB128) {
+                rm = new RijndaelManaged();
+                kSize = 256;
+                vSize = 128;
+                rm.KeySize = kSize;
+                rm.BlockSize = vSize;
+                rm.Mode = CipherMode.CFB;
+                mkey = InnerK;
+                mvector = InnerV;
+                encryptor = rm.CreateEncryptor(mkey, mvector);
+                decryptor = rm.CreateDecryptor(mkey, mvector);
+                encoder = new UTF8Encoding();
+            }
         }
 
         public bool FileOps(string File, string Mode, string Password) {
@@ -53,8 +117,8 @@ namespace AESEncryptAndDecrypt
 
         public void UpDateArrays() {
             rm = new RijndaelManaged();
-            rm.KeySize = 256;
-            rm.BlockSize = 256;
+            rm.KeySize = kSize;
+            rm.BlockSize = vSize;
             encryptor = rm.CreateEncryptor(mkey, mvector);
             decryptor = rm.CreateDecryptor(mkey, mvector);
             encoder = new UTF8Encoding();
@@ -66,7 +130,7 @@ namespace AESEncryptAndDecrypt
                 Retme = Convert.ToBase64String(Encrypt(encoder.GetBytes(unencrypted)));
             }
             catch (Exception ex) {
-                Retme = "ERROR " +ex.Message.ToString();
+                Retme = "ERROR " + ex.Message.ToString();
             }
             return Retme;
         }
@@ -82,10 +146,17 @@ namespace AESEncryptAndDecrypt
             return Retme;
         }
 
+        //public string EncryptToUrl(string unencrypted)
+        //{
+        //    return HttpUtility.UrlEncode(Encrypt(unencrypted));
+        //}
         public string EncryptAndReturn(string unencrypted) {
             return (Encrypt(unencrypted));
         }
-        
+        //public string DecryptFromUrl(string encrypted)
+        //{
+        //    return Decrypt(HttpUtility.UrlDecode(encrypted));
+        //}
         public string DecryptAndReturn(string encrypted) {
             return Decrypt(encrypted);
         }
@@ -107,9 +178,10 @@ namespace AESEncryptAndDecrypt
         }
 
         internal static bool EncryptFile(string inputPath, string outputPath, string password) {
+            var mMode = rm.Mode;
             var input = new FileStream(inputPath, FileMode.Open, FileAccess.Read);
             var output = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write);
-            var algorithm = new RijndaelManaged {KeySize = 256, BlockSize = 256};
+            var algorithm = new RijndaelManaged {KeySize = kSize, BlockSize = vSize, Mode = mMode};
             var key = new Rfc2898DeriveBytes(password, Encoding.ASCII.GetBytes(Salt));
  
             algorithm.Key = key.GetBytes(algorithm.KeySize/8);
@@ -122,11 +194,11 @@ namespace AESEncryptAndDecrypt
             return true;
         }
  
-        internal static bool DecryptFile(string inputPath, string outputPath, string password)
-        {
+        internal static bool DecryptFile(string inputPath, string outputPath, string password) {
+            var mMode = rm.Mode;
             var input = new FileStream(inputPath, FileMode.Open, FileAccess.Read);
             var output = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write);
-            var algorithm = new RijndaelManaged {KeySize = 256, BlockSize = 256};
+            var algorithm = new RijndaelManaged {KeySize = kSize, BlockSize = vSize, Mode = mMode};
             var key = new Rfc2898DeriveBytes(password, Encoding.ASCII.GetBytes(Salt));
  
             algorithm.Key = key.GetBytes(algorithm.KeySize/8);
